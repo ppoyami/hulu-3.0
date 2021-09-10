d647406ff52f3e12f74992bff5205301218fa293
intae kim <intaemon@intaeui-MacBookAir.local>
2021-09-10 13:12:11 +0900

    refactor: nav 하이라이팅 css 작성

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,23 +1,30 @@
 import { useRouter } from 'next/router';
-import tw from 'twin.macro';
+import tw, { css } from 'twin.macro';
 
 import { getCategory } from 'utils/requests';
 
 const styles = {
   nav: tw`relative`,
   list: tw`flex overflow-x-scroll scrollbar-hide text-2xl whitespace-nowrap px-10 sm:px-20 space-x-10 sm:space-x-20`,
-  list__item: tw`last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500`,
+  list__item: isCurrent =>
+    css`
+      ${tw`last:pr-24 cursor-pointer transition duration-100 
+  transform hover:scale-125 hover:text-white active:text-red-500`}
+      ${isCurrent && tw`transform scale-125 text-blue-800`}
+    `,
   fade: tw`absolute top-0 right-0 bg-gradient-to-l from-primary h-full w-1/12`,
 };
 
 export default function Nav() {
   const router = useRouter();
+  const { query } = router;
+
   return (
     <nav css={styles.nav}>
       <div css={styles.list}>
         {Object.entries(getCategory).map(([key, { title }]) => (
           <h2
-            css={styles.list__item}
+            css={styles.list__item(key === query?.genre)}
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
           >
